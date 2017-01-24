085442cdf0ac9be1df7a4f62483e18b7dc654749
yasminakada <y.kada@live.nl>
2017-01-24 13:37:14 +0100

    basis of the classes done

--- Dental.py
+++ Dental.py
@@ -7,16 +7,26 @@
 #		And have the code automatically find his way?
 # TODO: interface python
 
+
+# Using a graph:
 # http://stackoverflow.com/questions/4901815/object-of-custom-type-as-dictionary-key
 
+
+"""
+---------------------------------
+---------------------------------
+		MAINCLASSES
+---------------------------------
+---------------------------------
+"""
 
 class DentalState(object):
     """
     """
-    def __init__(self,name,status=false):
+    def __init__(self,name,status=False):
         """"""
         self.name = name
-        self.status = status
+        self.status = ["healthy", "unhealthy"]
 
     def changeStatus(self, s):
 		self.status = s
@@ -24,10 +34,10 @@ class DentalState(object):
 class DentalProblem(object):
     """
     """
-    def __init__(self, name, status=false):
+    def __init__(self, name, status=False):
         """"""
         self.name = name
-        self.status = status
+        self.status = [True,False]
 
     def changeStatus(self, s):
 		self.status = s
@@ -37,17 +47,140 @@ class DentalObservable(object):
     """
     def __init__(self):
         """"""
-        pass
+
 
 class Treatment(object):
     """
     """
-    def __init__(self):
+    def __init__(self, name):
         """"""
-        pass
+        self.name = name
+        self.status = [True,False]
+        self.time = range(5,95,5)
         
 class DentalModel(object):
 	"""
 	"""
 	def __init__(self):
 		pass
+
+"""
+---------------------------------
+---------------------------------
+		SUBCLASSES
+---------------------------------
+---------------------------------
+"""
+
+
+
+"""
+---------------------------------
+Dental State
+---------------------------------
+"""
+class StateGum(DentalState):
+	"""
+    """
+    def __init__(self):
+        """"""
+        self.state = ["normal", "slight inflammation", "severe inflammation"]
+        self.recession = [True,False]
+
+class StateEnamel(DentalState):
+	"""
+    """
+    def __init__(self):
+        """"""
+        self.state = ["normal", "cracked", "broken", "cavity"]
+
+class StatePulp(DentalState):
+	"""
+    """
+    def __init__(self):
+        """"""
+        self.state = ["normal", "inflamation"]
+
+
+class StateApex(DentalState):
+	"""
+    """
+    def __init__(self):
+        """"""
+        self.state = ["normal", "inflamation"]
+
+
+"""
+---------------------------------
+Dental Problems
+---------------------------------
+"""
+
+class ProblemTooth(DentalProblem):
+	"""
+	"""
+	pass
+
+class ProblemGums(DentalProblem):
+	"""
+	"""
+	pass
+
+class ProblemOther(DentalProblem):
+	"""
+	"""
+	pass
+
+
+
+"""
+---------------------------------
+Dental Observables
+---------------------------------
+"""
+class ObservablePain(DentalObservable):
+    """
+    """
+    def __init__(self):
+        """"""
+        # TODO: check pain symptom/sign diagram probably can remove symptom and sign and 
+        # leave it at subjective and objective observables
+        self.paintype = ["throbbing", "sharp"]
+        self.onset = ["recent","chronic"]
+        self.reaction_to_cold = ["increase","decrease","same"]
+        self.reaction_to_heat = ["increase","decrease","same"]
+        self.location = ["known","unknown"]
+        self.source = ["particular_tooth","multiple_teeth","outside_teeth"]
+        self.course = ["worsened_or_same", "reduced"]
+        self.pain_bending_stairs = [True,False]
+        self.duration = ["short","medium", "long"] # This might need some translation into measurable times
+
+class ObservableSwelling(DentalObservable):
+    """
+    """
+    def __init__(self):
+        """"""
+        self.status = [True,False]
+
+class ObservableTooth(DentalObservable):
+    """
+    """
+    def __init__(self):
+        """"""
+        self.number = range(1,32)# number? What does this number refer to?
+        self.recent_treatment_bump = [True,False]
+
+class ObservableGums(DentalObservable):
+    """
+    """
+    def __init__(self):
+        """"""
+        self.bleeding = [True,False]
+        self.colour = self.color = ["bright_red", "normal"]
+
+class ObservableCommonCold(DentalObservable):
+    """
+    """
+    def __init__(self):
+        """"""
+        self.status = [True,False]        
